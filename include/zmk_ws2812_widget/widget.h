2e6b36079cff6c0baa34695f29ee5dd54cfd35db
james-xenon <goodmailbox13@gmail.com>
2026-05-09 17:50:25 +0800

    Update widget.h

--- include/zmk_ws2812_widget/widget.h
+++ include/zmk_ws2812_widget/widget.h
@@ -3,17 +3,17 @@
 #include <stdbool.h>
 
 /**
- * @brief Indicate current battery status with WS2812 LED colors/patterns
+ * @brief Indicate current battery status with WS2812 LED colors/patterns.
  */
 void ws2812_indicate_battery(void);
 
 /**
- * @brief Indicate current connectivity status with WS2812 LED colors/patterns
+ * @brief Indicate current connectivity status with WS2812 LED colors/patterns.
  */
 void ws2812_indicate_connectivity(void);
 
 /**
- * @brief Indicate current layer with WS2812 LED colors/patterns
+ * @brief Indicate current layer with WS2812 LED colors/patterns.
  */
 void ws2812_indicate_layer(void);
 
