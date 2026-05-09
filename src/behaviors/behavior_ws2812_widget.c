c9ae03776cac392c2c023407f085524d317c59db
james-xenon <goodmailbox13@gmail.com>
2026-05-09 17:41:11 +0800

    Update behavior_ws2812_widget.c

--- src/behaviors/behavior_ws2812_widget.c
+++ src/behaviors/behavior_ws2812_widget.c
@@ -1,10 +1,12 @@
 #define DT_DRV_COMPAT zmk_behavior_ws2812_widget
 
 #include <zephyr/device.h>
-#include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
 
+#include <drivers/behavior.h>
+
 #include <zmk/behavior.h>
+#include <zmk/event_manager.h>
 
 #include <zmk_ws2812_widget/widget.h>
 
@@ -16,30 +18,48 @@ struct behavior_ws2812_wdg_config {
     bool indicate_layer;
 };
 
-static int __maybe_unused behavior_ws2812_wdg_init(const struct device *dev) { return 0; }
+static int __maybe_unused behavior_ws2812_wdg_init(const struct device *dev) {
+    return 0;
+}
 
+/*
+ * Binding parameter:
+ *   &ws2812_wdg 0 = manual layer blink
+ *   &ws2812_wdg 1 = manual battery blink
+ *   &ws2812_wdg 2 = toggle all WS2812 indication ON/OFF
+ *   &ws2812_wdg 3 = force indication ON
+ *   &ws2812_wdg 4 = force indication OFF
+ */
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
 #if IS_ENABLED(CONFIG_WS2812_WIDGET)
-    const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
-    const struct behavior_ws2812_wdg_config *cfg = dev->config;
-
+    switch (binding->param1) {
+    case 1:
 #if IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING) && IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_BATTERY)
-    if (cfg->indicate_battery) {
         ws2812_indicate_battery();
-    }
 #endif
-#if (IS_ENABLED(CONFIG_ZMK_USB) || IS_ENABLED(CONFIG_ZMK_BLE)) && IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_CONNECTIVITY)
-    if (cfg->indicate_connectivity) {
-        ws2812_indicate_connectivity();
-    }
-#endif
-#if (!IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)) && IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_LAYER_CHANGE)
-    if (cfg->indicate_layer) {
+        break;
+
+    case 2:
+        ws2812_toggle_indication_enabled();
+        break;
+
+    case 3:
+        ws2812_set_indication_enabled(true);
+        break;
+
+    case 4:
+        ws2812_set_indication_enabled(false);
+        break;
+
+    case 0:
+    default:
+#if IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_LAYER_CHANGE)
         ws2812_indicate_layer();
+#endif
+        break;
     }
 #endif
-#endif // IS_ENABLED(CONFIG_WS2812_WIDGET)
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
@@ -55,17 +75,17 @@ static const struct behavior_driver_api __maybe_unused behavior_ws2812_wdg_drive
     .locality = BEHAVIOR_LOCALITY_GLOBAL,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .get_parameter_metadata = zmk_behavior_get_empty_param_metadata,
-#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+#endif
 };
 
-#define WS2812_WDG_INST(n)                                                                          \
-    static struct behavior_ws2812_wdg_config behavior_ws2812_wdg_config_##n = {                     \
-        .indicate_battery = DT_INST_PROP(n, indicate_battery),                                      \
-        .indicate_connectivity = DT_INST_PROP(n, indicate_connectivity),                            \
-        .indicate_layer = DT_INST_PROP(n, indicate_layer),                                          \
-    };                                                                                              \
-    BEHAVIOR_DT_INST_DEFINE(n, behavior_ws2812_wdg_init, NULL, NULL, &behavior_ws2812_wdg_config_##n, \
-                            POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                       \
-                            &behavior_ws2812_wdg_driver_api);
+#define WS2812_WDG_INST(n)                                                                        \
+    static struct behavior_ws2812_wdg_config behavior_ws2812_wdg_config_##n = {                   \
+        .indicate_battery = DT_INST_PROP(n, indicate_battery),                                    \
+        .indicate_connectivity = DT_INST_PROP(n, indicate_connectivity),                          \
+        .indicate_layer = DT_INST_PROP(n, indicate_layer),                                        \
+    };                                                                                            \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_ws2812_wdg_init, NULL, NULL,                              \
+                            &behavior_ws2812_wdg_config_##n, POST_KERNEL,                         \
+                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ws2812_wdg_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(WS2812_WDG_INST)
