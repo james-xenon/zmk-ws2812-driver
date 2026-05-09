80b843f000cc6fda7fc3b15a2c1a0c9b55614230
james-xenon <goodmailbox13@gmail.com>
2026-05-09 17:51:29 +0800

    Update behavior_ws2812_widget.c

--- src/behaviors/behavior_ws2812_widget.c
+++ src/behaviors/behavior_ws2812_widget.c
@@ -6,17 +6,10 @@
 #include <drivers/behavior.h>
 
 #include <zmk/behavior.h>
-#include <zmk/event_manager.h>
 
 #include <zmk_ws2812_widget/widget.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
-struct behavior_ws2812_wdg_config {
-    bool indicate_battery;
-    bool indicate_connectivity;
-    bool indicate_layer;
-};
 
 static int __maybe_unused behavior_ws2812_wdg_init(const struct device *dev) {
     return 0;
@@ -70,22 +63,16 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 }
 
 static const struct behavior_driver_api __maybe_unused behavior_ws2812_wdg_driver_api = {
+    .locality = BEHAVIOR_LOCALITY_GLOBAL,
     .binding_pressed = on_keymap_binding_pressed,
     .binding_released = on_keymap_binding_released,
-    .locality = BEHAVIOR_LOCALITY_GLOBAL,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .get_parameter_metadata = zmk_behavior_get_empty_param_metadata,
 #endif
 };
 
 #define WS2812_WDG_INST(n)                                                                        \
-    static struct behavior_ws2812_wdg_config behavior_ws2812_wdg_config_##n = {                   \
-        .indicate_battery = DT_INST_PROP(n, indicate_battery),                                    \
-        .indicate_connectivity = DT_INST_PROP(n, indicate_connectivity),                          \
-        .indicate_layer = DT_INST_PROP(n, indicate_layer),                                        \
-    };                                                                                            \
-    BEHAVIOR_DT_INST_DEFINE(n, behavior_ws2812_wdg_init, NULL, NULL,                              \
-                            &behavior_ws2812_wdg_config_##n, POST_KERNEL,                         \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_ws2812_wdg_init, NULL, NULL, NULL, NULL, POST_KERNEL,     \
                             CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ws2812_wdg_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(WS2812_WDG_INST)
