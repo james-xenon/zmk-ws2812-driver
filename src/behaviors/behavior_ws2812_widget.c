039115bf23a83e03be01e8cff867f17e54dcf7d3
james-xenon <goodmailbox13@gmail.com>
2026-08-19 19:13:42 +0300

    Update behavior_ws2812_widget.c

--- src/behaviors/behavior_ws2812_widget.c
+++ src/behaviors/behavior_ws2812_widget.c
@@ -2,58 +2,67 @@
 
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
+
 #include <drivers/behavior.h>
+
 #include <zmk/behavior.h>
+
 #include <zmk_ws2812_widget/widget.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-static int __maybe_unused behavior_ws2812_wdg_init(const struct device *dev) { return 0; }
+static int __maybe_unused behavior_ws2812_wdg_init(const struct device *dev) {
+    return 0;
+}
 
+/*
+ * Binding parameter:
+ *   &ws2812_wdg 0 = manual layer blink
+ *   &ws2812_wdg 1 = manual battery blink (local half only)
+ *   &ws2812_wdg 2 = toggle all WS2812 indication ON/OFF
+ *   &ws2812_wdg 3 = force indication ON
+ *   &ws2812_wdg 4 = force indication OFF
+ *   &ws2812_wdg 5 = battery blink BOTH halves (left then right)
+ */
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
 #if IS_ENABLED(CONFIG_WS2812_WIDGET)
     ws2812_note_activity();
+
     switch (binding->param1) {
-    case 0:
-#if IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_LAYER_CHANGE)
-        ws2812_indicate_layer();
-#endif
-        break;
     case 1:
 #if IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING) && IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_BATTERY)
         ws2812_indicate_battery();
 #endif
         break;
+
     case 2:
         ws2812_toggle_indication_enabled();
         break;
+
     case 3:
         ws2812_set_indication_enabled(true);
         break;
+
     case 4:
         ws2812_set_indication_enabled(false);
         break;
+
     case 5:
 #if IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING) && IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_BATTERY)
         ws2812_indicate_battery_both();
 #endif
         break;
-    case 6:
-        /* Set persistent layer color on BOTH halves via GLOBAL locality.
-         * param2 = layer number. Color/range hardcoded to cyan left-half.
-         * param2=0 clears all persistent layers. */
-        if (binding->param2 == 0) {
-            ws2812_set_persistent_layer_color(0, 0x000000, 0, 0);
-        } else {
-            ws2812_set_persistent_layer_color(binding->param2, 0x00FFFF, 0, 21);
-        }
-        break;
+
+    case 0:
     default:
-        LOG_WRN("Unknown ws2812_wdg param1: %d", binding->param1);
-        return -ENOTSUP;
+#if IS_ENABLED(CONFIG_WS2812_WIDGET_SHOW_LAYER_CHANGE)
+        ws2812_indicate_layer();
+#endif
+        break;
     }
 #endif
+
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
@@ -71,8 +80,8 @@ static const struct behavior_driver_api __maybe_unused behavior_ws2812_wdg_drive
 #endif
 };
 
-#define WS2812_WDG_INST(n) \
-    BEHAVIOR_DT_INST_DEFINE(n, behavior_ws2812_wdg_init, NULL, NULL, NULL, POST_KERNEL, \
+#define WS2812_WDG_INST(n)                                                                        \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_ws2812_wdg_init, NULL, NULL, NULL, POST_KERNEL,           \
                             CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ws2812_wdg_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(WS2812_WDG_INST)
