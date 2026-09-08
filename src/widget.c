e2ebb3a7fdacb85f200c6edd32cc9631ec61312d
james-xenon <goodmailbox13@gmail.com>
2026-09-08 12:59:52 +0300

    qqqqq

--- src/widget.c
+++ src/widget.c
@@ -106,7 +106,7 @@ static const uint8_t __maybe_unused persistent_sync_layers[] = {
  * ------------------------------------------------------------------------- */
 #if WS2812_HALF_IS_LEFT
 #define CAPS_INDICATOR_START 0
-#define CAPS_INDICATOR_COUNT 1
+#define CAPS_INDICATOR_COUNT 2
 #define CAPS_INDICATOR_COLOR 0xFFFFFF
 #define HID_LED_CAPS_LOCK_BIT BIT(1)
 static bool caps_lock_active;
