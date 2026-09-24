218f5a8715b0576e1d60f68a3c192c194530bd68
james-xenon <goodmailbox13@gmail.com>
2026-09-24 21:47:26 +0300

    ssasas

--- src/rgb_idle_timer.c
+++ src/rgb_idle_timer.c
@@ -4,6 +4,7 @@
 #include <zmk_ws2812_widget/widget.h>
 
 #include <zmk/rgb_underglow.h>
+#include <zmk/behavior_queue.h>
 
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -17,7 +18,6 @@ static bool timer_enabled = true;
 static uint32_t timeout_minutes = 15;
 
 
-
 static void rgb_idle_timer_handler(struct k_timer *timer)
 {
     ARG_UNUSED(timer);
@@ -26,8 +26,45 @@ static void rgb_idle_timer_handler(struct k_timer *timer)
         return;
     }
 
+    ws2812_idle_broadcast(false);
+}
 
-    zmk_rgb_underglow_off();
+static void ws2812_idle_broadcast(bool on)
+{
+    struct zmk_behavior_binding binding = {
+
+        .behavior_dev =
+            DEVICE_DT_NAME(DT_NODELABEL(ws2812_idle_sync)),
+
+        .param1 = on ? 1 : 0,
+
+        .param2 = 0
+    };
+
+
+    struct zmk_behavior_binding_event event = {
+
+        .position = 0,
+
+        .timestamp = k_uptime_get()
+
+    };
+
+
+    zmk_behavior_queue_add(
+        &event,
+        binding,
+        true,
+        0
+    );
+
+
+    zmk_behavior_queue_add(
+        &event,
+        binding,
+        false,
+        10
+    );
 }
 
 
@@ -38,13 +75,6 @@ void ws2812_idle_timer_init(void)
         &rgb_idle_timer,
         rgb_idle_timer_handler,
         NULL
-    );
-
-
-    k_timer_start(
-        &rgb_idle_timer,
-        K_MINUTES(timeout_minutes),
-        K_FOREVER
     );
 }
 
@@ -57,7 +87,7 @@ void ws2812_idle_timer_reset(void)
     }
 
 
-    zmk_rgb_underglow_on();
+    ws2812_idle_broadcast(true);
 
 
     k_timer_start(
@@ -83,8 +113,6 @@ void ws2812_idle_timer_toggle(void)
 
         k_timer_stop(&rgb_idle_timer);
 
-        zmk_rgb_underglow_on();
-
     }
 }
 
@@ -108,7 +136,9 @@ void ws2812_idle_timeout_change(int8_t direction)
 
 
     if (timer_enabled) {
+
         ws2812_idle_timer_reset();
+
     }
 }
 
